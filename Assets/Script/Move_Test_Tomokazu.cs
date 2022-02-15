393fef8d0507bd45d927e56f3050eecf62b041d5
Yuutti0510 <yuichi-ura-k20h@std.kbc.ac.jp>
2022-02-15 13:02:07 +0900

    ただジャンプするだけ（とりあえず移動もできる）

---
+++ Assets/Script/Move_Test_Tomokazu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move_Test_Tomokazu : MonoBehaviour
+{
+    Vector3 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("Fire");
+        }
+    }
+
+    void Update()
+    {
+        const float Speed = 1f;
+        transform.Translate(move * Speed * Time.deltaTime);
+    }
+}

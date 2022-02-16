55e3e180b640ee5ee1a39ffddf9271831b40349e
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2022-02-16 14:00:03 +0900

    落下死の完成と今までのスクリプトをプレイヤーに付ける事で動作する物に変更

--- Assets/Script/RetrySystem.cs
+++ Assets/Script/RetrySystem.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(Player))] /* 残機の概念が完成したら残機スクリプトを必須化予定 */
+/* プレイヤーに付けるスクリプトに変更 */
 public class RetrySystem : MonoBehaviour
 {
-    [SerializeField] private GameObject player;/* 自機オブジェクトをインスペクターから入れる */
     [SerializeField] private GameObject retryPoint; /* リトライ時の開始位置指示用オブジェクト、リトライ時この位置から開始、最初にここに入ってるオブジェクトの位置からステージ開始 */
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,7 @@ public class RetrySystem : MonoBehaviour
 
     public void Retry()/* リトライさせたいタイミングで呼び出してほしい */
     {
-        player.transform.position = retryPoint.transform.position; /* 位置をリトライオブジェクトに揃える */
+        gameObject.transform.position = retryPoint.transform.position; /* 位置をリトライオブジェクトに揃える */
     }
 
     public void RetrayAreaUpdate(GameObject newRetryPoint)/* newRetryPointに新しいリトライ位置のゲームオブジェクトを入れる事でリトライ位置を更新する */

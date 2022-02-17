a3dc9ddb5e30dba9ca5076d634e0b3a30d13d800
itokazu <12006048-k20h@stu.kbc.ac.jp>
2022-02-17 14:48:33 +0900

    シューズタイプスクリプト

---
+++ Assets/Script/ShoesType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ShoesType
+{
+    Jump_Shoes, /*二段ジャンプができる靴*/
+    Speed_Shoes, /*移動が速くなる靴*/
+    Magnet_Shoes, /*壁にくっつける靴*/
+}

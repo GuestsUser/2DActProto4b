59726b893b955cef31caf75dcf1670c143a35a9a
YUTOKUBA <yuuto-kuba-20h@kbc.stu.ac.jp>
2022-02-17 13:33:24 +0900

    LocoCameraYのスクリプトにコメントを追加

--- Assets/Script/LockCameraY.cs
+++ Assets/Script/LockCameraY.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿/*カメラのY座標を固定するスクリプト*/
+using UnityEngine;
 using Cinemachine;
 
 [ExecuteInEditMode]
@@ -7,16 +8,16 @@ using Cinemachine;
 public class LockCameraY : CinemachineExtension
 {
     [Tooltip("カメラのY座標を固定する値")]
-    public float m_YPosition = 10;
+    public float m_YPosition = 10;      
 
     protected override void PostPipelineStageCallback(
-        CinemachineVirtualCameraBase vcam,
+        CinemachineVirtualCameraBase vcam,          /*cinemachineのvirtualcameraを使用する*/
         CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
         if (stage == CinemachineCore.Stage.Body)
         {
             var pos = state.RawPosition;
-            pos.y = m_YPosition;
+            pos.y = m_YPosition;        /*Y座標をインスペクター欄で設定した値に固定*/
             state.RawPosition = pos;
         }
     }

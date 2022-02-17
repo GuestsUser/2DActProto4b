4e085aa3ddb23ccc6785c9125bf82aa54a9b6598
Yuutti0510 <yuichi-ura-k20h@std.kbc.ac.jp>
2022-02-17 13:57:29 +0900

    no message

--- Assets/Script/JumpSystem.cs
+++ Assets/Script/JumpSystem.cs
@@ -113,7 +113,7 @@ public class JumpSystem : Padinput
         /*レイキャストの使い方がわからないのでお願いします（プレイやの当たり判定をレイキャストにする）参照元： https://getabakoclub.com/2020/05/11/unity%e3%81%a7%e5%9c%b0%e9%9d%a2%e3%81%ae%e5%bd%93%e3%81%9f%e3%82%8a%e5%88%a4%e5%ae%9a%e3%82%92%e8%b6%b3%e5%85%83%e3%81%a0%e3%81%91%e5%8f%96%e5%be%97%e3%81%99%e3%82%8b%e3%80%903d%e3%80%91/ */
         rayPosition = rb.transform.position;/*レイキャストの位置*/
         /*レイキャストの位置,レイキャストの角度*/
-        ray = new Ray(rayPosition, transform.up * -1f);
+        ray = new Ray(rayPosition, -transform.up);
         ray2 = new Ray(rayPosition, new Vector3(0.5f, -1f, 0));
         ray3 = new Ray(rayPosition, new Vector3(0, -1f, 0.5f));
         ray4 = new Ray(rayPosition, new Vector3(-0.5f, -1f, 0));
@@ -127,9 +127,9 @@ public class JumpSystem : Padinput
 
         print(jump);/*デバッグログ,ジャンプフラグを出力*/
         /*脳筋式レイキャストの当たり判定処理*/
-        if (Physics.Raycast(ray, out rayHit, rayDistance) || Physics.Raycast(ray2, out rayHit, rayDistance)
+        if ((Physics.Raycast(ray, out rayHit, rayDistance) || Physics.Raycast(ray2, out rayHit, rayDistance)
             || Physics.Raycast(ray3, out rayHit, rayDistance) || Physics.Raycast(ray4, out rayHit, rayDistance)
-            || Physics.Raycast(ray5, out rayHit, rayDistance))
+            || Physics.Raycast(ray5, out rayHit, rayDistance))/*&&*/)
         {
 
             /*コライダーを持つオブジェクトから、タグを読み取る（地面オブジェクトをgroundに設定）*/

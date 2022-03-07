47f8dfc62483d5fca6b02b9d5e2a91445f559058
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2022-03-07 13:21:57 +0900

    滑る床とharukox軸移動を可能にする回転実装

--- Assets/Script/SlopeFloor.cs
+++ Assets/Script/SlopeFloor.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class SlopeFloor : MonoBehaviour
 {
+    [SerializeField] float force=0.5f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionStay(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
-            other.gameObject.GetComponent<Rigidbody>().AddForce(-10, 0, 0, ForceMode.Acceleration);
+            
+            Vector3 angle = transform.rotation.eulerAngles;
+            other.gameObject.GetComponent<Rigidbody>().AddForce(force * Mathf.Cos(angle.z * Mathf.Deg2Rad), force * Mathf.Sin(angle.z * Mathf.Deg2Rad), 0, ForceMode.VelocityChange);
         }
     }
 }

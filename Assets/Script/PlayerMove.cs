4799332a54a9ed19e57f2ef76a9ac60ee0b60531
itokazu <12006048-k20h@stu.kbc.ac.jp>
2022-03-15 10:47:33 +0900

    プレイヤーのアニメーションと走る時の加速処理を合わせた

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -49,6 +49,7 @@ public class PlayerMove : Padinput
 
     //アニメーション用
     [SerializeField] Animator animator;
+    float anim_speed;
 
     /*Kuttukuで使うプレイヤーポジション*/
     //[SerializeField] GameObject player;
@@ -61,7 +62,7 @@ public class PlayerMove : Padinput
 
     public float height;
 
-    private void Start()
+    private void Start() /*初期化*/
     {
         this.transform.localRotation = Quaternion.Euler(0, 0, 0);
         max_move_x = 13f;
@@ -73,8 +74,12 @@ public class PlayerMove : Padinput
         //move = Vector3.zero;
         idle = true;
         height = 0;
+
+        anim_speed = 0.1f;
     }
-    public override void Move()
+    
+    /*オーバーライド関数(自動で呼び出される 呼び出しタイミングはコントローラー割り当てがされているボタン、スティックが入力された時)*/
+    public override void Move() /*動かす時の左スティック入力状態をここで取得*/
     {
         if (Gamepad.current.leftStick.x.ReadValue() > 0)
         {
@@ -113,23 +118,20 @@ public class PlayerMove : Padinput
         
 
     }
-    public override void MoveStop()
+    public override void MoveStop()/*プレイヤーの動きを止める処理*/
     {
         
         state = State.idle; /*止まっている*/
         move_x = 0;
         move = Vector3.zero;
-    }
-    void Update()
-    {
-        //rayPosition = transform.position /*+ new Vector3(0,1,0)*/;
-        //player_front = new Ray(rayPosition, transform.right * ray_direction);
-        //Debug.DrawRay(rayPosition, player_front.direction * rayDistance, Color.blue);
 
-        //if(kuttuku.collider_exit == true)
-        //{
-        //    move_x = 5;
-        //}
+        /*歩きアニメーションに戻すための処理*/
+        anim_speed = 0.1f;
+    }
+    /*オーバーライド関数*/
+
+    void Update() /*常に処理する内容*/
+    {
 
         if (state == State.idle)
         {
@@ -139,70 +141,77 @@ public class PlayerMove : Padinput
         {
             idle = false;
         }
+
         ApplyAnimator();
-        if (kuttuku.bool_ray_hit == false)
+        RunAndTurn();
+    }
+    
+    private void RunAndTurn() /*走る+振り向き処理*/
+    {
+        if (kuttuku.bool_ray_hit == false) /*くっつき状態ではない場合*/
         {
             /*下の処理の効果：くっついた際にプレイヤーが下を向いてしまう問題を改善*/
-            /*3月7日追加部分*/
             bool_left_direction = true;
             bool_right_direction = true;
-            /*3月7日追加部分*/
 
-            if (right != 0)
+            if (right != 0)/*左スティックが右に入力されているとき*/
             {
-                player_direction = Quaternion.Euler(0, 0, 0);
-                transform.localRotation = player_direction;
+                /*プレイヤーの向きを右に向いている状態にする処理*/
+                player_direction = Quaternion.Euler(0, 0, 0); /*Quaternion.Eulerで向きを3軸(xyz)まとめて値を指定したものをプレイヤーの向きを入れる変数に代入*/
+                transform.localRotation = player_direction; /*プレイヤーの向きをlocalRotationに代入して回転させる*/
             }
-            else if (left != 0)
+            else if (left != 0)/*左スティックが左に入力されているとき*/
             {
-                player_direction = Quaternion.Euler(0, 180, 0);
-                transform.localRotation = player_direction;
+                /*プレイヤーの向きを左に向いている状態にする処理*/
+                player_direction = Quaternion.Euler(0, 180, 0); /*Quaternion.Eulerで向きを3軸(xyz)まとめて値を指定したものをプレイヤーの向きを入れる変数に代入*/
+                transform.localRotation = player_direction; /*プレイヤーの向きをlocalRotationに代入して回転させる*/
             }
         }
-        else if(kuttuku.bool_ray_hit == true)
+        else if (kuttuku.bool_ray_hit == true)/*くっつき状態の場合*/
         {
-            Debug.Log("今はくっついてます");
-            
-
-            if (right != 0)
+            if (right != 0) /*左スティックが右に入力されている場合*/
             {
-                bool_left_direction = false;
-                if (bool_right_direction == false)
+                bool_left_direction = false; /*左に振り向く時に使うフラグをfalseに*/
+                if (bool_right_direction == false) /*右に振り向く時に必要なフラグがfalseの場合*/
                 {
-                    Quaternion rot = Quaternion.AngleAxis(180, Vector3.up);
-                    Quaternion q = this.transform.localRotation;
+                    /*【回転させる準備】 rotには回転させたい値と軸を指定したものを、qには現在のlocalRotationの値を代入*/
+                    Quaternion rot = Quaternion.AngleAxis(180, Vector3.up); /*y軸で180°回転するように指定 ※x軸:Vector3.right y軸:Vector3.up z軸:Vector3.foward*/
+                    Quaternion q = this.transform.localRotation; /*これがないと現在の値から+〇度回転させた値にするということが出来ない*/
+                    /*【回転させる準備】*/
 
-                    this.transform.localRotation = q * rot;
-                    bool_right_direction = true;
+                    /*localRotationに値を代入し、実際に回転させる*/
+                    this.transform.localRotation = q * rot;/*【q * rot】にすることで現在の値から〇°回転という処理が出来る*/
+                    bool_right_direction = true; /*プレイヤーが右に向いているときのフラグをtrueに*/
 
                 }
-                
+
 
             }
-            else if (left != 0)
+            else if (left != 0)/*左スティックが左に入力されている場合*/
             {
-                bool_right_direction = false;
-                if (bool_left_direction == false)
+                bool_right_direction = false; /*プレイヤーが右に向いている時にtrueになるフラグをfalseに*/
+                if (bool_left_direction == false) /*プレイヤーが左に向いているフラグがfalseの時*/
                 {
-                    if (this.transform.localRotation.y != -90f)
+                    if (this.transform.localRotation.y != -90f) /*もし、localRotation.yの値が-90fじゃない時*/
                     {
-                        Quaternion rot = Quaternion.AngleAxis(180, Vector3.up);
-                        Quaternion q = this.transform.localRotation;
+                        /*【回転させる準備】 rotには回転させたい値と軸を指定したものを、qには現在のlocalRotationの値を代入*/
+                        Quaternion rot = Quaternion.AngleAxis(180, Vector3.up); /*y軸で180°回転するように指定*/
+                        Quaternion q = this.transform.localRotation; /*現在の値を保持*/
+                        /*【回転させる準備】*/
 
-                        this.transform.localRotation = q * rot;
-                        bool_left_direction = true;
+                        /*localRotationに値を代入し、実際に回転させる*/
+                        this.transform.localRotation = q * rot; /*【q * rot】にすることで現在の値から〇°回転という処理が出来る*/
+                        bool_left_direction = true;/*プレイヤーが左に向いているときのフラグをtrueに*/
                     }
-                    
+
                 }
-                
+
 
             }
         }
 
         Debug.Log(state);/*プレイヤーの状態*/
-        //Debug.Log(right);
-        //Debug.Log(transform.localRotation.y);
-        //Debug.Log(input_abs);
+
         switch (state)
         {
             case State.idle: /*止まっている時*/
@@ -226,31 +235,39 @@ public class PlayerMove : Padinput
             move = Vector3.zero;
         }
         transform.Translate((move / 10) * speed * Time.deltaTime);
-        
-            
+
+
     }
+
     private void RunAccel() /*走る時の加速処理*/
     {
         /*追加部分*/ /*プレイヤーを回転させる場合これのみでok*/
-        run_time += Time.deltaTime;
-        if (run_time < eas_time)
+        run_time += Time.deltaTime; /*run_timeを少しずつ動かす*/
+        if (run_time < eas_time) /*緩急をつけるトータルの時間よりrun_timeが小さければまだ加速処理*/
         {
-            move_x = ExpOut(run_time, eas_time, 2f, max_move_x);
+            move_x = ExpOut(run_time, eas_time, 2f, max_move_x); /*move_xに2f～max_move_xの値を入れる処理(加速)*/
+            
+            /*↓プレイヤーの加速処理とアニメーションを合わせる為に追加した処理*/
+            anim_speed = ExpOut(run_time, eas_time, 0.1f, 1f); /*ブレンドツリーに代入するスピードの値を0.1f～１fで加速させる処理*/
         }
-        else
+        else /*緩急をつける時間を越えたら*/
         {
-            move_x = max_move_x;
+            move_x = max_move_x; /*最大値を維持*/
+            
+            /*↓プレイヤーの加速処理とアニメーションを合わせる為に追加した処理*/
+            anim_speed = 1f; /*走りのアニメーションを維持するために1を代入*/
         }
     }
-    public static float ExpOut(float t, float totaltime, float min, float max) /*加速関数*/
+    public static float ExpOut(float t, float totaltime, float min, float max) /*加速処理に使う関数*/
     {
         max -= min;
         return t == totaltime ? max + min : max * (-Mathf.Pow(2, -10 * t / totaltime) + 1) + min;
     }
     void ApplyAnimator()
     {
-        var speed = Mathf.Abs(input_abs);
-        animator.SetFloat("Speed", speed, 0.1f, Time.deltaTime);
+        //var speed = Mathf.Abs(input_abs);
+        //animator.SetFloat("Speed", speed, 0.1f, Time.deltaTime);
+        animator.SetFloat("Speed", anim_speed, 0.1f, Time.deltaTime);
 
         //if (speed == 0)
         //{

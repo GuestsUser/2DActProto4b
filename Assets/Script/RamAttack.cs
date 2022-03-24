716cabea4ee89bb9d8b6bc76899f9b97e3b18795
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2022-03-24 12:52:40 +0900

    敵に触れるとダメージを受けるようになった

---
+++ Assets/Script/RamAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 自機接触でダメージを与えるだけ */
+public class RamAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") { DamageSystem.Damage(0.5f, DamageSystem.DamageCombo.damage); }
+    }
+}

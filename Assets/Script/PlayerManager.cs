880e9ae5de0386548fabb03a3b7e1bf9af9b72bc
KBC12006160\kbc12006160 <ryu-ookouchi_20h@stu.kbc.ac.jp>
2022-04-03 01:18:46 +0900

    タグItem追加、プレイヤープレハブにItem取得用スクリプト追加、Itemプレハブの作成、Item用スクリプト作成

---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    private int count;
+    private int zanki;
+    public Text itemCountText;
+
+    private void Start()
+    {
+        count = 0;
+    }
+
+    private void Update()
+    {
+        /*アイテム獲得数表示更新*/
+        itemCountText.text = "コイン数：" + count.ToString() + " / 100";
+
+        if(count >= 2)
+        {
+            zanki++;
+            Debug.Log(zanki);
+            count = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*プレイヤーが触れたオブジェクトのタグがItemなら*/
+        if (other.gameObject.tag == "Item")
+        {
+            /*Itemタグが付いているオブジェクトを消す*/
+            other.gameObject.GetComponent<ItemManager>().GetItem();
+            /*カウント+1*/
+            count++;
+        }
+    }
+}

2120b79e334aaef9c24ecbf03e1563d3db31a21e
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2022-04-11 11:06:23 +0900

    滑る床と新ダッシュ暫定版、床の端に乗ると滑らない問題未解決

---
+++ Assets/Script/JumpFlgReset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* プレイヤーに付けて使う */
+[RequireComponent(typeof(JumpSystem))] /* ジャンプ必須化 */
+public class JumpFlgReset : MonoBehaviour
+{
+    private JumpSystem jump; /* ジャンプシステム */
+    // Start is called before the first frame update
+    void Start()
+    {
+        jump = GetComponent<JumpSystem>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        jump.completion = false; /* 一番最後に実行する事で押された瞬間だけtrueとして処理を書ける */
+    }
+}

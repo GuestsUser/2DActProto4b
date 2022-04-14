9273393f49dd1aeabd9c2af8fc84c4da5cdce18d
itokazu <12006048-k20h@stu.kbc.ac.jp>
2022-04-14 13:12:57 +0900

    ポーズUIの最前面に表示させるスクリプトと、ポーズと操作説明をまとめたプレハブを作成した

---
+++ Assets/ChangeObjNum.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeObjNum : MonoBehaviour
+{
+    int sibling_index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sibling_index = transform.GetSiblingIndex();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.SetAsLastSibling();
+    }
+}

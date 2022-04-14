6dae7415b00b7fc207e1d07e6892f95b2d5fccec
YUTOKUBA <yuuto-kuba-20h@kbc.stu.ac.jp>
2022-04-14 14:56:00 +0900

    no message

--- Assets/Script/EnemyKillAside.cs
+++ Assets/Script/EnemyKillAside.cs
@@ -8,9 +8,20 @@ public class EnemyKillAside : MonoBehaviour
     public Vector3 rayPosition; /*レイキャストの位置*/
 
     /*レイキャスト用変数*/
-    public Ray ray;
+    public Ray ray1;
+    public Ray ray2;
+    public Ray ray3;
+
+    public Transform legtransform1;
+    public Transform legtransform2;
+    public Transform legtransform3;
+
+    public Vector3 legposition1;
+    public Vector3 legposition2;
+    public Vector3 legposition3;
+
     public RaycastHit rayHit;
-    [SerializeField] public float rayDistance = 0.1f;
+    [SerializeField] public float rayDistance = 0.1f;   /*raycastの長さ*/
 
     /*リジッドボディー*/
     public Rigidbody rb;
@@ -26,22 +37,33 @@ public class EnemyKillAside : MonoBehaviour
 
     void Update()
     {
+        /*レイキャスト*/
 
-        rayPosition = rb.transform.position; /*レイキャストの位置*/
-        /*レイキャストの位置*/
-        ray = new Ray(rayPosition, transform.up * -1f);
+        legposition1 = legtransform1.position;
+        legposition2 = legtransform2.position;
+        legposition3 = legtransform3.position;
+
+        /*位置*/
+
+        ray1 = new Ray(legposition1, -legtransform1.up);
+        ray2 = new Ray(legposition2, -legtransform2.up);
+        ray3 = new Ray(legposition3, -legtransform3.up);
+
+        Debug.DrawRay(legposition1, ray1.direction * rayDistance,Color.yellow);
+        Debug.DrawRay(legposition2, ray2.direction * rayDistance,Color.red);
+        Debug.DrawRay(legposition3, ray3.direction * rayDistance,Color.blue) ;
+       
 
 
-        /*デバッグ用の可視光線*/
-        Debug.DrawRay(rayPosition, ray.direction * rayDistance, Color.yellow);
-        /*レイキャスト当たり判定処理*/
 
-        if (Physics.Raycast(ray,out rayHit, rayDistance)){
-            if(rayHit.collider.tag == "Enemy")
+
+        if (Physics.Raycast(ray1, out rayHit, rayDistance) || Physics.Raycast(ray2, out rayHit, rayDistance) || Physics.Raycast(ray3, out rayHit, rayDistance))
+        {
+            if (rayHit.collider.tag == "Enemy")
             {
                 rayHit.collider.gameObject.SetActive(false);/*エネミーを非アクティブ状態にする*/
 
-                rb.velocity = new Vector3(0,0,0);
+                rb.velocity = new Vector3(0, 0, 0);
                 rb.AddForce(new Vector3(0, upForce, 0));    /*敵を踏んだら上にジャンプ*/
 
             }

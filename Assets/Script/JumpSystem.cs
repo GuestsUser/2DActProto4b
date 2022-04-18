4f81bb9dace6ad25a8ba760022429c5040a9fd2d
Yuutti0510 <yuichi-ura-k20h@std.kbc.ac.jp>
2022-04-18 14:04:12 +0900

    レイの長さを調整

--- Assets/Script/JumpSystem.cs
+++ Assets/Script/JumpSystem.cs
@@ -49,7 +49,7 @@ public class JumpSystem : Padinput
     /*レイキャスト修正版*/
 
     /*（レイキャスト）可視光線の長さ*/
-    [SerializeField] public float rayDistance = 0.05f;
+    public float rayDistance = 0.08f;
 
     /*靴のテストフラグ（ステージ攻略で靴を切り替える）*/
     public static bool jumpFlg_Test = false;   /*二弾ジャンプにさせたい場合このフラグにtrueを入れる*/

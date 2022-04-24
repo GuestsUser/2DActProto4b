54b407255d1936f2e58ccd9a8fef67a7c745a488
YUTOKUBA <yuuto-kuba-20h@kbc.stu.ac.jp>
2022-04-24 23:36:54 +0900

    ゲージをダッシュフラグと紐づけ

--- Assets/Script/gauge.cs
+++ Assets/Script/gauge.cs
@@ -8,7 +8,7 @@ public class gauge : MonoBehaviour
 {
     [Header("ゲージを動かすスクリプト（新）kuba")]
 
-    [SerializeField] private ChangeShoes change_shoes;
+    public DashSystemN dashsystem;
 
     Image fillImg;
     float timeAmt = 5;  /*ゲージの稼働時間*/
@@ -17,8 +17,6 @@ public class gauge : MonoBehaviour
 
     void Start()
     {
-        //change_shoes = GetComponent<ChangeShoes>();
-
         fillImg = this.GetComponent<Image>();   /*ゲージの画像*/
         time = timeAmt;
     }
@@ -27,7 +25,7 @@ public class gauge : MonoBehaviour
     {
 
 
-        if (Gamepad.current.buttonWest.wasPressedThisFrame && GaugeFlg == false)    /*Xボタンを押す＋フラグがfalseなら*/
+        if (Gamepad.current.buttonWest.wasPressedThisFrame && dashsystem.CoolTimeFlg == true && GaugeFlg == false)    /*Xボタンを押す＋ CoolTimeフラグがtrueなら + ゲージフラグがfalseなら */
         {
             GaugeFlg = true;        /*ゲージのフラグをONに*/
             fillImg.fillAmount = 0.0f;  /*ゲージの画像を非表示*/
@@ -42,12 +40,36 @@ public class gauge : MonoBehaviour
 
         if (fillImg.fillAmount == 1f)
         {
-            /*Debug.Log("aaaaaaa");*/
             GaugeFlg = false;
             timeAmt = 5;
-            //fillImg.fillAmount = 1.0f;
             time += 5;
         }
 
+
+
+        /*************************************************　古いやつ（一応残している））**************************************************************/
+
+        //if (Gamepad.current.buttonWest.wasPressedThisFrame && GaugeFlg == false)    /*Xボタンを押す＋フラグがfalseなら*/
+        //{
+        //    GaugeFlg = true;        /*ゲージのフラグをONに*/
+        //    fillImg.fillAmount = 0.0f;  /*ゲージの画像を非表示*/
+        //}
+
+
+        //if (GaugeFlg)   /*ゲージのフラグがtrueなら*/
+        //{
+        //    fillImg.fillAmount += 1.0f / timeAmt * Time.deltaTime;  /*ゲージの画像を少しずつ表示*/
+        //    /*fillAmountは1～0の間で動くので　1 / countTimeの時間で表示がMAXになるように(1 / 5 で5秒間)*/
+        //}
+
+        //if (fillImg.fillAmount == 1f)
+        //{
+        //    /*Debug.Log("aaaaaaa");*/
+        //    GaugeFlg = false;
+        //    timeAmt = 5;
+        //    //fillImg.fillAmount = 1.0f;
+        //    time += 5;
+        //}
+        /**************************************************************************************************************************************************/
     }
 }

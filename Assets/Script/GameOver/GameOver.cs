b25b11a832504e55576cab78e6444c2cd3405c4f
itokazu <12006048-k20h@stu.kbc.ac.jp>
2022-04-26 13:30:53 +0900

    GameOverの演出を追加

--- Assets/Script/GameOver/GameOver.cs
+++ Assets/Script/GameOver/GameOver.cs
@@ -8,19 +8,34 @@ public class GameOver : MonoBehaviour
     private GameOverMenu Gom;
     private GameObject gameOver;
 
+    private Text gO_text;
+    [Tooltip("不透明度(デバッグ用に表示してます)")][SerializeField] private float opacity; 
     void Start()
     {
         Gom = GetComponent<GameOverMenu>();
         gameOver = GameObject.Find("GameOver");
+        gO_text = gameOver.GetComponent<Text>();
         /*アイテム数の初期化*/
         GManager.instance.itemNum = 0;
         /*残機数の初期化*/
         GManager.instance.zankiNum = 3;
+
+        opacity = 0;
     }
 
     void Update()
     {
         taiki();
+        
+        if(++opacity < 100)
+        {
+            ;
+        }
+        else if(opacity > 100)
+        {
+            opacity = 100;
+        }
+        gO_text.color = new Color(1, 0, 0, opacity / 100);
     }
 
     private void taiki()
@@ -31,7 +46,8 @@ public class GameOver : MonoBehaviour
     IEnumerator GameOverMenu()
     {
         yield return new WaitForSecondsRealtime(3);  //処理を待機
-        gameOver.SetActive(false);
+        //gameOver.SetActive(false);
+        
         Gom.show_menu = true;
     }
 }

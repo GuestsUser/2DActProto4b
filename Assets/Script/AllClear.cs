3f3f538fccceef21756fadd0975415f18e4d0506
itokazu <12006048-k20h@stu.kbc.ac.jp>
2022-04-29 14:48:30 +0900

    3ステージ全てクリアすると、AllClearシーンに行けるようになった

---
+++ Assets/Script/AllClear.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class AllClear : MonoBehaviour
+{
+    int WaitTime = 2;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+        /*ゲームパッドのBボタンが押されたら*/
+        if (Gamepad.current.buttonSouth.wasPressedThisFrame)
+        {
+            StartCoroutine("BacktoTitle");
+        }
+
+    }
+    private IEnumerator BacktoTitle() //シーンチェンジ用
+    {
+        yield return new WaitForSecondsRealtime(WaitTime);  //処理を待機
+
+        SceneManager.LoadScene("Title"); //タイトルに戻る実装する場合 Scene名はTitleなのでここはそのままにしてあります
+        
+    }
+}

68124e4889d4284c1cffaa73dfdfb341c1b14002
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2022-05-19 15:57:58 +0900

    壊れる床に触れた時の色を分かりやすい物に変更

--- Assets/Script/BreakFloor.cs
+++ Assets/Script/BreakFloor.cs
@@ -9,7 +9,8 @@ public class BreakFloor : MonoBehaviour
     [Tooltip("乗ってから崩れるまでの時間、秒指定")] [SerializeField] float notice = 1;
     [Tooltip("この時間分経過すると復活、秒指定")] [SerializeField] float respawn = 5;
     [Tooltip("この変数にマテリアルが入っていると乗った時、消えるまでの時間オブジェクトをこのマテリアルに変更")] [SerializeField] Material noticeMaterial;
-    [Tooltip("乗った瞬間の演出時間")] [SerializeField] float whiteEffectTime = 0.05f;
+    [Tooltip("乗った瞬間の演出用マテリアル")] [SerializeField] Material whiteEffectMat;
+    [Tooltip("乗った瞬間の演出時間")] [SerializeField] float whiteEffectTime = 0.04f;
 
     List<GameObject> allObj; /* このオブジェクトと子のオブジェクト全て */
     List<Color32> childColor; /* 子のオブジェクトの元の色 */
@@ -67,7 +68,6 @@ public class BreakFloor : MonoBehaviour
             float limit = flg ? respawn : notice; /* フラグの状態によって用いるべき時間の決定 */
 
             if (!flg) { StartCoroutine(WhiteEffect()); }
-
             for (float i = 0; i < limit;) /* 待機 */
             {
                 i += Time.deltaTime;
@@ -77,26 +77,10 @@ public class BreakFloor : MonoBehaviour
             if (flg)
             {
                 ColorReset();
-                if (noticeMaterial != null)
-                {
-                    for (int i = 0; i < allObj.Count; i++)
-                    {
-                        if (isRender[i]) { allObj[i].GetComponent<MeshRenderer>().material = originMat; }
-                    }
-                }
+                if (noticeMaterial != null) { MaterialChange(originMat); }
             }
             else { ColorChange(clear); }
             for (int i = 0; i < allObj.Count; i++) { if (isCollider[i]) { allObj[i].GetComponent<Collider>().enabled = flg; } } /* 所属オブジェクトの当たり判定の削除と復活 */
-            //int sub = 0; /* childColorはmeshrendererがついてないオブジェクトは飛ばすのでallObjと添え字が合わない事態を回避する為独自に添え字を用意してた */
-            //for (int i= 0; i < allObj.Count; i++) /* 所属オブジェクトの透明度と当たり判定の削除 */
-            //{
-            //    if (isRender[i])
-            //    {
-            //        allObj[i].GetComponent<MeshRenderer>().material.color = flg ? childColor[sub] : clear;
-            //        sub++;
-            //    }
-            //    if (isCollider[i]) { allObj[i].GetComponent<Collider>().enabled = flg; }
-            //}
         }
         useFlg = false; /* 解放 */
     }
@@ -105,20 +89,21 @@ public class BreakFloor : MonoBehaviour
     {
         float count = 0;
 
-        if (noticeMaterial != null)
-        {
-            for (int i = 0; i < allObj.Count; i++)
-            {
-                if (isRender[i]) { allObj[i].GetComponent<MeshRenderer>().material = noticeMaterial; }
-            }
-        }
-        ColorChange(white);
+        if (noticeMaterial != null) { MaterialChange(whiteEffectMat); }
         while (count < whiteEffectTime)
         {
             count += Time.deltaTime;
             yield return StartCoroutine(TimeScaleYield.TimeStop());
         }
-        ColorReset();
+        MaterialChange(noticeMaterial);
+    }
+
+    void MaterialChange(Material mat)
+    {
+        for (int i = 0; i < allObj.Count; i++)
+        {
+            if (isRender[i]) { allObj[i].GetComponent<MeshRenderer>().material = mat; }
+        }
     }
 
     void ColorChange(Color32 c)

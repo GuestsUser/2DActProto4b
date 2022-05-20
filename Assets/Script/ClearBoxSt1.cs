68f63620654ad7e909177b6bf44a0da8c24d48fa
YUTOKUBA <yuuto-kuba-20h@kbc.stu.ac.jp>
2022-05-20 15:18:11 +0900

    Stage2にクリアオブジェクトを追加、クリアオブジェクトを切り替えるスクリプトを各ステージごとに分割

---
+++ Assets/Script/ClearBoxSt1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearBoxSt1 : MonoBehaviour
+{
+    public StageClear StageClear;
+
+    /*各ステージ宝箱の状態*/
+    [SerializeField] private GameObject Stage1TB;
+    [SerializeField] private GameObject Stage1TBOpen;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        /*ステージのクリアフラグがたつとポールのみのオブジェクトが非アクティブ化され、旗のフラグがアクティブ化される*/
+
+        if (StageClear._isStage1Clear)
+        {
+            Stage1TB.gameObject.SetActive(false);
+            Stage1TBOpen.gameObject.SetActive(true);
+        }
+    }
+}
